e2126028ec311118398ccb15a339e5258e4476ea
Zeeshan Noor <zeeshannoorofficial@gmail.com>
2022-12-05 18:51:53 +0500

    'home-url'

--- src/frontend/Header.jsx
+++ src/frontend/Header.jsx
@@ -9,7 +9,7 @@ function Header(){
 	<nav className="autohide navbar navbar-expand-md navbar-dark">
 	<div className="container-fluid" >
 		<div className="animbrand">
-		<a className="navbar-brand animate" href="/">Noor</a>
+		<a className="navbar-brand animate" href="https://noorsir.github.io/KK-React-Practice/">Noor</a>
 		</div>
 	<button className="navbar-toggler" style={{boxShadow:"unset"}} type="button" data-bs-toggle="collapse"  data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation">
 		<span className="navbar-toggler-icon"></span>
@@ -17,7 +17,7 @@ function Header(){
 	<div className="collapse navbar-collapse justify-content-end" id="main_nav">
 
 		<ul className="navbar-nav mx-auto">
-			<li className="nav-item active dropdown" ><a className="nav-link " data-toggle="dropdown" href="/"> Home</a></li>
+			<li className="nav-item active dropdown" ><a className="nav-link " data-toggle="dropdown" href="https://noorsir.github.io/KK-React-Practice/"> Home</a></li>
 			<li onClick="getId()" className="nav-item dropdown"><a className="nav-link"  href="#about-myself"> About Me </a></li>
 			<li  className="nav-item"><a id="skills" className="nav-link" href="#myskills"  > My Skills </a></li>
 			<li className="nav-item"><a id="portfolio" className="nav-link" href="#myportfolio" > Portfolio </a></li>
